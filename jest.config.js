b6620da9f23704281a52eae7635eb3c8828dff3b
Ayomidemi <adenijiayomide13@gmail.com>
2025-07-28 18:10:07 +0100

    Fix test and build

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,7 @@ export default {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/__mocks__/fileMock.js",
+    "^@/(.*)$": "<rootDir>/src/$1",
   },
   testMatch: [
     "<rootDir>/src/**/__tests__/**/*.{ts,tsx}",
@@ -30,10 +31,7 @@ export default {
       "ts-jest",
       {
         useESM: true,
-        tsconfig: {
-          jsx: "react-jsx",
-          esModuleInterop: true,
-        },
+        tsconfig: "tsconfig.test.json",
       },
     ],
   },
